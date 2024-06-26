44d8e7760c35adf3ef8c7a0c9aa9f6fc279faa83
osajustin <osajustin@gmail.com>
2024-06-26 18:31:24 -0400

    styled projects section

--- components/FeaturedProjects/FeaturedProjects.tsx
+++ components/FeaturedProjects/FeaturedProjects.tsx
@@ -1,42 +1,43 @@
-// components/FeaturedProjects.js
-import { Container, Grid, Card, Image, Text, Group, Badge, GridCol, Divider } from '@mantine/core';
+import { Container, Grid, Card, Image, Text, Group, GridCol, Divider, Flex } from '@mantine/core';
 import projects from './../../public/projects.json';
 import { FaGithub, FaLink } from 'react-icons/fa';
+import classes from './FeaturedProjects.module.css';
 
 const FeaturedProjects = () => {
   return (
-    <Container>
-      <Text size="xl" style={{ marginBottom: '20px' }}>
-        Featured Projects
+    <Container className={classes.featuredProjects}>
+      <Text className={classes.sectionTitle}>
+        FEATURED PROJECTS
       </Text>
       <Grid justify="space-between">
         {projects.map((project, index) => (
           <GridCol key={index} span={6} style={{ display: 'flex', justifyContent: 'center' }}>
-            <Card shadow="sm" padding="lg" radius="md" style={{ width: '100%', maxWidth: '540px' }}>
+            <Card radius="md" style={{ width: '100%', maxWidth: '540px' }}>
+              
               <Card.Section>
-                <Image src={project.image} alt={project.title} height={160} />
+                <Image className={classes.image} src={project.image} alt={project.title} height={160} />
               </Card.Section>
-                <Grid>
-                    <GridCol span={6}>
-                        <Text>{project.title}</Text>
-                    </GridCol>
-                    <GridCol span={3} offset={3}>
-                        <FaGithub />
-                        <FaLink />
-                    </GridCol>
-                </Grid>
+
+              <Card.Section>
+                <Group justify="space-between" align="stretch">
+                    <Text className={classes.projectTitle}>{project.title}</Text>
+                  
+                  <Group>
+                      <FaGithub />
+                      <FaLink />
+                  </Group>
+                </Group>
+                <Text className={classes.projectDescription}>
+                  {project.description}
+                </Text>
+                <Text className={classes.projectTags}>
+                  {project.tags.join(' - ')}
+                </Text>
+              </Card.Section>
+             
                 
                 
-              <Text size="sm" style={{ color: '#666' }}>
-                {project.description}
-              </Text>
-              <Group style={{ marginTop: 10 }}>
-                {project.tags.map((tag, idx) => (
-                  <Badge key={idx} color="blue" variant="light">
-                    {tag}
-                  </Badge>
-                ))}
-              </Group>
+              
             </Card>
           </GridCol>
         ))}
