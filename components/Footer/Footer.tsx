e24b31628c32a5d3068394881a8272ee7436830f
osajustin <osajustin@gmail.com>
2024-07-06 14:52:54 -0400

    Added styling for footer small display

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,70 +1,59 @@
-import { Container, Group, Text, Grid, GridCol, Button, Flex, SimpleGrid } from '@mantine/core';
+import React from 'react';
+import { Container, Group, Text, Button, Flex } from '@mantine/core';
 import { FaEnvelope, FaFile, FaGithub, FaLinkedin } from 'react-icons/fa';
 import classes from './Footer.module.css';
 
 export function Footer() {
   return (
-        <Container size='md' className={classes.footerContainer}>
-          <SimpleGrid cols={{ base: 2, sm: 1}} spacing={"lg"}>
-
-            {/* SectionTitle */}
-            <div>
-              <Text className={classes.sectionTitle}>
-                CONTACT
-              </Text>
-            </div>
-
-            {/* Group of Buttons */}
-            <Flex 
-              direction={{ base: 'column', sm: 'row' }}
-              gap={{ base: 'sm', sm: 'lg' }}
-              justify={{ base: 'space-between', sm: 'center' }}>
-              <Button
-                  justify="center"
-                  className={classes.button}
-                  component="a"
-                  href="https://github.com/osajustin"
-                  leftSection={<FaGithub />}
-                  mt="md"
-                >    
-                  Github     
-                </Button>
-                
-                
-                <Button
-                  justify="center"
-                  className={classes.button}
-                  component="a"
-                  href="https://www.linkedin.com/in/justinosagie"
-                  leftSection={<FaLinkedin />}
-                  mt="md"
-                >
-                  LinkedIn
-                </Button>
-                <Button
-                  justify="center"
-                  className={classes.button}
-                  component="a"
-                  href="/public/OsagieJustin_CV.pdf"
-                  download
-                  leftSection={<FaFile />}
-                  mt="md"
-                >
-                  Resume
-                </Button>
-                <Button
-                  justify="center"
-                  className={classes.button}
-                  component="a"
-                  href="mailto:osajustin@gmail.com"
-                  leftSection={<FaEnvelope />}
-                  mt="md"
-                >
-                  Send an Email
-                </Button>
-              </Flex>
-            
-          </SimpleGrid>
-        </Container>
+    <Container size='md' className={classes.footerContainer}>
+      <Flex justify="space-between" align="center" className={classes.contactSection}>
+        <Text className={classes.sectionTitle}>
+          CONTACT
+        </Text>
+        <Group className={classes.buttonGroup}>
+          <Button
+            justify="center"
+            className={classes.button}
+            component="a"
+            href="https://github.com/osajustin"
+            leftSection={<FaGithub />}
+            mt="md"
+          >
+            Github
+          </Button>
+          <Button
+            justify="center"
+            className={classes.button}
+            component="a"
+            href="https://www.linkedin.com/in/justinosagie"
+            leftSection={<FaLinkedin />}
+            mt="md"
+          >
+            LinkedIn
+          </Button>
+          <Button
+            justify="center"
+            className={classes.button}
+            component="a"
+            href="/public/OsagieJustin_CV.pdf"
+            download
+            leftSection={<FaFile />}
+            mt="md"
+          >
+            Resume
+          </Button>
+          <Button
+            justify="center"
+            className={classes.button}
+            component="a"
+            href="mailto:osajustin@gmail.com"
+            leftSection={<FaEnvelope />}
+            mt="md"
+          >
+            Send an Email
+          </Button>
+        </Group>
+      </Flex>
+    </Container>
   );
 }
